f5f5a30417b26640fe39ed38f397f9c23fa4dc98
Nahyun-Kim <50420981+NahyunEE@users.noreply.github.com>
2024-07-07 00:21:00 +0900

    ServerUpdate
    놀랍게도 코드가 존재했다 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

---
+++ nodejs/elastic/server.js
@@ -0,0 +1,16 @@
+const { Client } = require('@elastic/elasticsearch')
+const config = require('config');
+const elasticConfig = config.get('elastic');
+
+const client = new Client({
+  cloud: {
+    id: elasticConfig.cloudID
+  },
+  auth: {
+    username: elasticConfig.username,
+    password: elasticConfig.password
+  }
+})
+
+
+
